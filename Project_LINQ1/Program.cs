82e8c26f6954cf24fa6ab833232a857e26cf91af
sheikhimtiaz <imticorei54@gmail.com>
2020-07-13 08:32:09 +0600

    a few changes

--- Project_LINQ1/Program.cs
+++ Project_LINQ1/Program.cs
@@ -6,14 +6,21 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
+
+// 
+// see the code changes and tell me what differences do you notice.
+// keep learning. keep posting updates about your progress
+// if you have problem understanding any concepts, message me in facebook. i will prepare myself to talk about the topic
+//
+
 namespace Project_LINQ1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var list1 = new List<Student>();
-            list1.Add(new Student
+            var listOfStudents = new List<Student>();
+            listOfStudents.Add(new Student
             {
                 StudentId = 01,
                 StudentName = "Abc",
@@ -23,7 +30,7 @@ namespace Project_LINQ1
                               new Subject { SubjectName = "english", SubjectMark = 70 } 
                 }
             });
-            list1.Add(new Student
+            listOfStudents.Add(new Student
             {
                 StudentId = 02,
                 StudentName = "Bcd",
@@ -33,7 +40,7 @@ namespace Project_LINQ1
                               new Subject { SubjectName = "biology", SubjectMark = 95 }
                 }
             });
-            list1.Add(new Student
+            listOfStudents.Add(new Student
             {
                 StudentId = 03,
                 StudentName = "Cde",
@@ -43,7 +50,7 @@ namespace Project_LINQ1
                               new Subject { SubjectName = "chemistry", SubjectMark = 100 }
                 }
             });
-            list1.Add(new Student
+            listOfStudents.Add(new Student
             {
                 StudentId = 04,
                 StudentName = "Def",
@@ -53,7 +60,7 @@ namespace Project_LINQ1
                               new Subject { SubjectName = "chemistry", SubjectMark = 80 }
                 }
             });
-            list1.Add(new Student
+            listOfStudents.Add(new Student
             {
                 StudentId = 05,
                 StudentName = "Efg",
@@ -63,7 +70,7 @@ namespace Project_LINQ1
                               new Subject { SubjectName = "chemistry", SubjectMark = 100 }
                 }
             });
-            list1.Add(new Student
+            listOfStudents.Add(new Student
             {
                 StudentId = 06,
                 StudentName = "Fgh",
@@ -73,7 +80,7 @@ namespace Project_LINQ1
                               new Subject { SubjectName = "biology", SubjectMark = 50 }
                 }
             });
-            list1.Add(new Student
+            listOfStudents.Add(new Student
             {
                 StudentId = 07,
                 StudentName = "Ghi",
@@ -83,27 +90,38 @@ namespace Project_LINQ1
                               new Subject { SubjectName = "chemistry", SubjectMark = 100 }
                 }
             });
-            var list2 = new List<Subject>();
-            list2.Add(new Subject { SubjectName = "math", SubjectMark = 100 });
-            list2.Add(new Subject { SubjectName = "physics", SubjectMark = 100 });
-            list2.Add(new Subject { SubjectName = "chemistry", SubjectMark = 100 });
-            list2.Add(new Subject { SubjectName = "biology", SubjectMark = 100 });
-            list2.Add(new Subject { SubjectName = "english", SubjectMark = 100 });
-            list2.Add(new Subject { SubjectName = "arts", SubjectMark = 100 });
-            foreach (var lis1 in list1)
+            var listOfSubjects = new List<Subject>();
+            listOfSubjects.Add(new Subject { SubjectName = "math", SubjectMark = 100 });
+            listOfSubjects.Add(new Subject { SubjectName = "physics", SubjectMark = 100 });
+            listOfSubjects.Add(new Subject { SubjectName = "chemistry", SubjectMark = 100 });
+            listOfSubjects.Add(new Subject { SubjectName = "biology", SubjectMark = 100 });
+            listOfSubjects.Add(new Subject { SubjectName = "english", SubjectMark = 100 });
+            listOfSubjects.Add(new Subject { SubjectName = "arts", SubjectMark = 100 });
+            foreach (var lis1 in listOfStudents)
             {
-                Console.WriteLine($"{lis1.StudentId}------{lis1.StudentName}------{lis1.TotalMarks}");
+                Console.WriteLine($"{lis1.StudentId}\t{lis1.StudentName}\t{lis1.TotalMarks}\n");
                 foreach(var em in lis1.EachMarks)
                 {
-                    Console.WriteLine($"{em.SubjectName}-----------{em.SubjectMark}");
+                    var tabSpaces = em.SubjectName == "chemistry" ? "\t" : "\t\t";
+                    Console.WriteLine($"{em.SubjectName}{tabSpaces}{em.SubjectMark}");
                 }
-                Console.WriteLine("-------------------------------------------------------------------");
+                Console.WriteLine("\n\n");
             }
-            var output1 = (from li1 in list1
+            var output1 = (from li1 in listOfStudents
                            from li11 in li1.EachMarks
                            where li11.SubjectName == "math" && li11.SubjectMark > 80
                            select li1).ToList();
-            var output2 = (from li1 in list1
+            Console.WriteLine("Variable output1 value:");
+            Console.WriteLine(output1);
+            foreach(var item in output1)
+            {
+                Console.WriteLine($"{item.StudentName} : Total Marks ({item.TotalMarks})");
+                foreach(var marks in item.EachMarks)
+                {
+                    Console.WriteLine($"{marks.SubjectName}\t{marks.SubjectMark}");
+                }
+            }
+            var output2 = (from li1 in listOfStudents
                            select li1).Contains(new Student
                            {
                                StudentId = 04,
@@ -114,14 +132,21 @@ namespace Project_LINQ1
                                new Subject { SubjectName = "chemistry", SubjectMark = 80 }
                            }
                            }, new StudentCompare());
+            Console.WriteLine("\n\nVariable output2 value for total marks 240:");
             Console.WriteLine(output2);
-            var list3 = new List<Teacher>();
-            list3.Add(new Teacher { TeacherId = 01, TeacherName = "zy", TeacherSubject = "math" });
-            list3.Add(new Teacher { TeacherId = 02, TeacherName = "yx", TeacherSubject = "chemistry" });
-            list3.Add(new Teacher { TeacherId = 03, TeacherName = "xw", TeacherSubject = "physics" });
-            list3.Add(new Teacher { TeacherId = 04, TeacherName = "wv", TeacherSubject = "biology" });
-            list3.Add(new Teacher { TeacherId = 05, TeacherName = "vu", TeacherSubject = "english" });
-            list3.Add(new Teacher { TeacherId = 06, TeacherName = "ut", TeacherSubject = "sports" });
+            // todo change total marks to 245. see the value changes. 
+            //from now on, if you have problem anywhere, if you dont understand any particular piece of code, 
+            // comment where your confusion is. add comments in the top or bottom 
+
+
+
+            var listOfTeachers = new List<Teacher>();
+            listOfTeachers.Add(new Teacher { TeacherId = 01, TeacherName = "zy", TeacherSubject = "math" });
+            listOfTeachers.Add(new Teacher { TeacherId = 02, TeacherName = "yx", TeacherSubject = "chemistry" });
+            listOfTeachers.Add(new Teacher { TeacherId = 03, TeacherName = "xw", TeacherSubject = "physics" });
+            listOfTeachers.Add(new Teacher { TeacherId = 04, TeacherName = "wv", TeacherSubject = "biology" });
+            listOfTeachers.Add(new Teacher { TeacherId = 05, TeacherName = "vu", TeacherSubject = "english" });
+            listOfTeachers.Add(new Teacher { TeacherId = 06, TeacherName = "ut", TeacherSubject = "sports" });
             var list5 = new List<Teacher>();
             var list4 = new List<Student>();
             list5.Add(new Teacher { TeacherId = 01, TeacherName = "zy", TeacherSubject = "math" });
@@ -136,9 +161,9 @@ namespace Project_LINQ1
                               new Subject { SubjectName = "chemistry", SubjectMark = 100 }
                 }
             });
-            var output3 = list3.Intersect(list5).ToList();
-            var output5 = list1.Except(list4).ToList();
-            var output4 = (from lis1 in list1
+            var output3 = listOfTeachers.Intersect(list5).ToList();
+            var output5 = listOfStudents.Except(list4).ToList();
+            var output4 = (from lis1 in listOfStudents
                            select new
                            {
                                StudentName = lis1.StudentName,
@@ -153,17 +178,17 @@ namespace Project_LINQ1
                                             TotalMarks = lis4.TotalMarks,
                                             //EachMarks = new List<Subject>() //lis4.EachMarks
                                         }).ToList();
-            /*foreach(var lis2 in list2)
+            /*foreach(var lis2 in listOfSubjects)
             {
                 Console.WriteLine(lis2.SubjectName+"<<<<<>>>>>"+lis2.SubjectMark);
             }
             Console.WriteLine("-----------------------------------------------");
-            foreach(var lis3 in list3)
+            foreach(var lis3 in listOfTeachers)
             {
                 Console.WriteLine(lis3.TeacherId+"<<<>>>"+lis3.TeacherName+"<<<>>>"+lis3.TeacherSubject);
             }*/
-            var output6 = (from lis3 in list3
-                           join lis2 in list2 
+            var output6 = (from lis3 in listOfTeachers
+                           join lis2 in listOfSubjects 
                            on lis3.TeacherSubject equals lis2.SubjectName
                            select new
                            {
@@ -172,7 +197,7 @@ namespace Project_LINQ1
                            }).ToList();
             foreach (var out6 in output6)
             {
-                Console.WriteLine(out6.teacher + "<<<>>>" + out6.number);
+                Console.WriteLine("\n\n\n\n"+out6.teacher + "<<<>>>" + out6.number);
             }
             var list6 = new List<Relation>();
             list6.Add(new Relation { Id = 1, studentCount = 3 });
@@ -183,9 +208,9 @@ namespace Project_LINQ1
             list6.Add(new Relation { Id = 7, studentCount = 0 });
 
             var output7 = (from lis6 in list6
-                           join lis3 in list3
+                           join lis3 in listOfTeachers
                            on lis6.Id equals lis3.TeacherId
-                           join lis2 in list2
+                           join lis2 in listOfSubjects
                            on lis3.TeacherSubject equals lis2.SubjectName
                            select new
                            {
@@ -195,7 +220,7 @@ namespace Project_LINQ1
                                studentCount = lis6.studentCount,
                                mark = lis2.SubjectMark
                            }).ToList();
-            var output8 = (from lis1 in list1
+            var output8 = (from lis1 in listOfStudents
                            from li1 in lis1.EachMarks
                            group li1 by li1.SubjectName into subject
                            select new
@@ -203,8 +228,8 @@ namespace Project_LINQ1
                                name = subject.Key,
                                count = subject.Count()
                            }).ToList();
-            var output9 = (from lis2 in list2
-                           join lis3 in list3
+            var output9 = (from lis2 in listOfSubjects
+                           join lis3 in listOfTeachers
                            on lis2.SubjectName equals lis3.TeacherSubject
                            into tempList
                            from temp in tempList.DefaultIfEmpty()
@@ -218,7 +243,7 @@ namespace Project_LINQ1
             {
                 Console.WriteLine(out9.subject + "=-=-=-=-=-=" + out9.teacher);
             }
-            var output10 = (from lis3 in list3
+            var output10 = (from lis3 in listOfTeachers
                             join lis6 in list6
                             on lis3.TeacherId equals lis6.Id
                             into listTemp
